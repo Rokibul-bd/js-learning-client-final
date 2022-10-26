006e2dc01fa9a1e965718efe7bc1978534b2b781
Rokibul-bd <mdrokbiulislamcmt176@gmail.com>
2022-10-27 02:10:12 +0600

    google provider log in system add and image conditionaly add

--- src/Components/register/Register.js
+++ src/Components/register/Register.js
@@ -1,7 +1,23 @@
+import { GoogleAuthProvider } from 'firebase/auth';
 import React from 'react';
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 
 const Register = () => {
+
+    const { googleSignIn } = useContext(AuthContext);
+    const googleProvider = new GoogleAuthProvider();
+
+    const handleGoogleSignIn = () => {
+        googleSignIn(googleProvider)
+            .then(result => {
+                const user = result.user
+                console.log(user)
+            })
+            .catch(error => console.error(error))
+    }
+
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col">
@@ -14,19 +30,19 @@ const Register = () => {
                             <label className="label">
                                 <span className="label-text">Full Name</span>
                             </label>
-                            <input type="text" placeholder="Name" className="input input-bordered" />
+                            <input type="text" placeholder="Name" name="name" className="input input-bordered" />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="text" placeholder="email" className="input input-bordered" />
+                            <input type="text" placeholder="email" name="email" className="input input-bordered" />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="text" placeholder="password" className="input input-bordered" />
+                            <input type="text" placeholder="password" name="password" className="input input-bordered" />
                             <label className="mt-2">
                                 Already have an account please <Link to="/login" className="text-base link link-hover">log in</Link>
                             </label>
@@ -34,6 +50,7 @@ const Register = () => {
                         <div className="form-control mt-6">
                             <button className="btn btn-primary">Sign Up</button>
                         </div>
+                        <button onClick={handleGoogleSignIn} className='btn btn-sm'>Google Sign In</button>
                     </div>
                 </div>
             </div>
--- src/contexts/AuthProvider/AuthProvider.js
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { createContext } from 'react';
-
+import { getAuth, onAuthStateChanged, signInWithPopup } from 'firebase/auth'
+import app from '../../firebase/firebase.config'
+import { useState } from 'react';
 
 export const AuthContext = createContext();
-const user = { displayName: "Md Rokibul" }
-const authInfo = { user }
+const auth = getAuth(app)
+
+
 const AuthProvider = ({ children }) => {
+
+    const [user, setUser] = useState(null)
+
+    const googleSignIn = (googleProvider) => {
+        signInWithPopup(auth, googleProvider)
+    }
+    useEffect(() => {
+        const unsubcribe = onAuthStateChanged(auth, (currentUser) => {
+            console.log("user inside state Change  : ", currentUser);
+            setUser(currentUser)
+        });
+        return () => {
+            unsubcribe();
+        }
+    }, [])
+    const authInfo = { user, googleSignIn }
     return (
         <AuthContext.Provider value={authInfo}>
             {children}
